d6eb70a4c7073ed1d3e7b94b5594c17056a605ad
Kristiyan17 <kristiyan.kkirilov@gmail.com>
2020-04-30 23:33:19 +0300

    Sound settings script

---
+++ Game_everything/Script/SoundManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SoundManager
+{
+    public static void PlaySound()
+    {
+        GameObject soundGameObject = new GameObject("Sound");
+    }
+
+}
